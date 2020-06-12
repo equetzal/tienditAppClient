7cc2046dd34132e561303691c54327678932bce8
equetzal <eithnegomez@hotmail.com>
2020-06-11 20:41:34 -0500

    Added register, dashboard and setup empty screens

---
+++ app/src/main/java/mx/ipn/escom/tienditappclient/activities/dashboard.kt
@@ -0,0 +1,12 @@
+package mx.ipn.escom.tienditappclient.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.ipn.escom.tienditappclient.R
+
+class dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
