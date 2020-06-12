7886661cd5b009b9fe50f8b62ea2f3dac74f8956
equetzal <eithnegomez@hotmail.com>
2020-06-11 20:40:23 -0500

    Added load screen

--- app/src/main/java/mx/ipn/escom/tienditappclient/MainActivity.kt
+++ app/src/main/java/mx/ipn/escom/tienditappclient/MainActivity.kt
@@ -1,11 +1,21 @@
 package mx.ipn.escom.tienditappclient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import mx.ipn.escom.tienditappclient.activities.login
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
     }
 }
